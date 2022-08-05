ef0fd5ab6a0e3e7b5e73c37a0e23a6a1f363deed
DavidVFiumano <tpt.fiumano@gmail.com>
2022-08-04 22:15:10 -0400

    Maybe working init

--- hideAndSeekPrototype.py
+++ hideAndSeekPrototype.py
@@ -1,12 +1,24 @@
 ﻿from math import sqrt
+from datetime import now
 
 import viz
 import vizact
 import vizinput
 import steve
 
+# CONSTANTS
+START_TIMER_ID = 0
 GAME_END_THRESHOLD = 0.65
+START_TIME_COUNTDOWN = 10
 
+# game state variables
+gameStarted = False
+vizardLoadTime = now()
+screenText = viz.addText("Waiting for other player to connect...", parent=SCREEN)
+startCountDown = START_TIME_COUNTDOWN
+gameRole = None
+
+# vizard code below this line
 viz.setMultiSample(4)
 viz.fov(60)
 viz.MainView.collision( viz.ON )
@@ -27,13 +39,16 @@ target_machine = vizinput.input('Enter the name of the other machine').upper()
 #Add a mailbox from which to send messages. This is your outbox.
 target_mailbox = viz.addNetwork(target_machine)
 
-def sendPosition():
+def sendUpdates():
 
-    #Retrieve current transform of viewpoint
-    mat = viz.MainView.getMatrix()
+    if gameStarted:
+        #Retrieve current transform of viewpoint
+        mat = viz.MainView.getMatrix()
 
-    #Send position/rotation to target network object
-    target_mailbox.send(action=updateGameState, quat=mat.getQuat(), pos=mat.getPosition())
+        #Send position/rotation to target network object
+        target_mailbox.send(action=updateGameState, quat=mat.getQuat(), pos=mat.getPosition())
+    else:
+        target_mailbox.send(action=startGame, otherStartTime=vizardLoadTime)
 
 # Start a timer that sends out data over the network every frame
 vizact.ontimer(0,sendPosition)
@@ -56,5 +71,42 @@ def onNetwork(e):
     if e.sender.upper() == target_machine:
         e.action(e)
 
+# handles any and all timers
+def onTimer(timerID):
+    if timerID == START_TIMER_ID:
+        if startCountDown > 0:
+            screenText.message = f"{startCountDown}..."
+            startCountDown -= 1
+        elif startCountDown == 0:
+            screenText.message = f"You are the {gameRole}. Go!"
+            startCountDown = -1
+            viz.mouse.setOverride(state=viz.OFF)
+            gameStarted = True
+        elif startCountDown == -1:
+            screenText.message = ""
+
+def startCountdownTimer(e):
+    if e.sender.upper() == target_machine:
+        viz.starttimer(timer=START_TIMER_ID, time=1, repeats=START_TIME_COUNTDOWN)
+    
+
+def decideRole(otherStartTime):
+    if vizardLoadTime < otherStartTime:
+        gameRole = "Seeker"
+    else:
+        gameRole = "Hider"
+
+def startGame(e):
+    decideRole(e.otherStartTime)
+    startCountdownTimer(e)
+
+# Causes the game to wait for a player to connect.
+def waitForPlayer(e):
+    print("Waiting...")
+    viz.addText("Waiting for other player to connect...", parent=viz.SCREEN)
+    viz.mouse.setOverride()
+    
+
 # Register network to listen from incomming messages
 viz.callback(viz.NETWORK_EVENT, onNetwork)
+viz.callback(viz.INIT_EVENT, waitForPlayer)
